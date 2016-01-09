d4c3913016a47407cbf6d286d6d2df0d64f671b1
EchoWine <dimitri.who@gmail.com>
2016-01-10 00:26:34 +0100

    Renamed rollback to restore; Improved restore system with (prepare,save,undo,restore)

--- DataBase.class.php
+++ DataBase.class.php
@@ -22,14 +22,14 @@ class DB{
 	protected static $log;
 
 	/**
-	 * Contains the last ID of the table and it's used for rollback
+	 * Contains the last ID of the table and it's used for restore
 	 */
-	public static $rollbackLastID;
+	public static $restoreLastID;
 
 	/**
-	 * Contains the last name of the table and it's used for rollback
+	 * Contains the last name of the table and it's used for restore
 	 */
-	public static $rollbackLastTable;
+	public static $restoreLastTable;
 	
 	/**
 	 * Create a new connection
@@ -56,7 +56,7 @@ class DB{
 		}
 
 		self::select($cfg['database']);
-		self::iniRollback();
+		self::iniRestore();
 
 	}
 	
@@ -243,143 +243,6 @@ class DB{
 		return self::$con -> lastInsertId();
 	}
 
-
-	/**
-	 * Create the table that handle the rollback
-	 */
-	private static function iniRollback(){
-
-		if(!self::$config['alter_schema'])return;
-		
-		self::query("
-			CREATE TABLE IF NOT EXISTS database_rollback(
-				id BIGINT(11) auto_increment,
-				table_rollback varchar(55),
-				table_from varchar(55),
-				primary key (id)
-			);
-		");
-
-		self::_delete();
-	}
-
-	/**
-	 * Save the current status of the table
-	 *
-	 * @param string $table name of the table
-	 */
-	public static function save($table){
-		self::$rollbackLastTable = self::_save($table);
-	}
-
-	/**
-	 * Confirm the last operation
-	 */
-	public static function commit(){
-		self::_delete();
-	}
-
-	/**
-	 * Bring back the status of a table before the last save
-	 */
-	public static function undo(){
-		$table = self::$rollbackLastTable;
-		
-		$q = self::query("SELECT * FROM database_rollback WHERE table_rollback = '{$table}'");
-		$a = $q -> fetch();
-		self::_restore($a['table_from'],$a['table_rollback']);
-
-		self::query("DROP TABLE {$table}");
-		self::query("DELETE FROM database_rollback WHERE table_rollback = '{$table}'");
-	}
-
-	/**
-	 * Save a table
-	 *
-	 * @param string $table name of the table
-	 */
-	private static function _save($table){
-		do{
-			$name = md5(microtime());
-			$name = "database_rollback_{$name}";
-		}while(false);
-
-		self::query("INSERT INTO database_rollback (table_rollback,table_from) VALUES ('{$name}','{$table}')");
-		self::$rollbackLastID = self::getInsertID();
-		self::query("CREATE TABLE {$name} LIKE {$table}");
-		self::query("INSERT {$name} SELECT * FROM {$table}");
-
-		return $name;
-
-	}
-	
-	/**
-	 * Delete the last saved operation
-	 */
-	private static function _delete(){
-
-		// Cancellare l'ultima istanza
-		$l = self::$config['rollback'] - 1;
-		$q = self::query("SELECT table_rollback FROM database_rollback ORDER BY id ASC");
-		if(self::count($q) > self::$config['rollback']){
-			$a = $q -> fetch();
-			self::query("DROP table {$a['table_rollback']}");
-			self::query("DELETE FROM database_rollback WHERE table_rollback = '{$a['table_rollback']}'");
-		}
-
-	}
-
-	/**
-	 * Restore a table
-	 *
-	 * @param string $t1 name of table to restore
-	 * @param stirng $t2 name of table to take data
-	 * @return bool result of the query
-	 */
-	private static function _restore($t1,$t2){
-
-		return self::query("TRUNCATE table {$t1}") && 
-		self::query("INSERT {$t1} SELECT * FROM {$t2}");
-	}
-
-	/**
-	 * Execute a rollback
-	 *
-	 * @param int $n number of operations to going back
-	 * @param int $id ID of the operation from which start
-	 * @param bool $overwrite overwrite the records during the rollback
-	 * @return bool result of the operation
-	 */
-	public static function rollback($n = 1,$id = NULL,$overwrite = true){
-
-		if($n < 1) $n = 1;
-		$n--;
-
-		$w = isset($id) && !empty($id) ? "WHERE id = {$id} ORDER BY id DESC " : " ORDER BY id DESC LIMIT {$n},1";
-
-		$q = self::query("SELECT * FROM database_rollback {$w}");
-		if(self::count($q) == 1){
-			$a = $q -> fetch();
-			
-			self::_save($a['table_from']);
-			
-			if($overwrite){
-				self::_restore($a['table_from'],$a['table_rollback']);
-			}else{
-				$q1 = true;
-				$q2 = self::query("
-					INSERT IGNORE {$a['table_from']} 
-					SELECT * FROM {$a['table_rollback']} 
-					ON DUPLICATE KEY UPDATE {$a['table_from']}.id = {$a['table_from']}.id
-				");
-			}
-
-			self::_delete();
-			return $q1 && $q2;
-		}
-		return false;
-	}
-
 	/**
 	 * Check if a table exists
 	 *
@@ -401,5 +264,121 @@ class DB{
 		return new QueryBuilder($v,$as);
 	}
 
+	// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+	// 
+	//		RESTORE: PREPARE, SAVE, UNDO
+	// 
+	// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+	/**
+	 * Create the table that will handle the restore
+	 */
+	private static function iniRestore(){
+
+		if(!self::$config['alter_schema'])return;
+		
+		self::query("
+			CREATE TABLE IF NOT EXISTS db_restore(
+				id BIGINT(11) auto_increment,
+				table_restore varchar(55),
+				table_from varchar(55),
+				primary key (id)
+			);
+		");
+
+		self::save();
+	}
+
+	/**
+	 * Prepare the table 
+	 * Save the current status of the table
+	 *
+	 * @param string $table name of the table
+	 */
+	public static function prepare($table){
+
+		do{
+			$name = md5(microtime());
+			$name = "db_restore_{$name}";
+		}while(false);
+
+		self::query("INSERT INTO db_restore (table_restore,table_from) VALUES ('{$name}','{$table}')");
+		self::$restoreLastID = self::getInsertID();
+		self::query("CREATE TABLE {$name} LIKE {$table}");
+		self::query("INSERT {$name} SELECT * FROM {$table}");
+		self::$restoreLastTable = $name;
+	}
+
+	/**
+	 * Save the operation
+	 * Delete the last operation and "confirm" the actual already entered
+	 */
+	public static function save(){
+		
+		
+		$l = self::$config['restore'] - 1;
+		$q = self::query("SELECT table_restore FROM db_restore ORDER BY id ASC");
+		if(self::count($q) > self::$config['restore']){
+			$a = $q -> fetch();
+			self::query("DROP table {$a['table_restore']}");
+			self::query("DELETE FROM db_restore WHERE table_restore = '{$a['table_restore']}'");
+		}
+	}
+
+	/**
+	 * Bring back the records of a table before the prepare
+	 */
+	public static function undo(){
+		$table = self::$restoreLastTable;
+		
+		$q = self::query("SELECT * FROM db_restore WHERE table_restore = '{$table}'");
+		$a = $q -> fetch();
+		self::_restore($a['table_from'],$a['table_restore']);
+
+		self::query("DROP TABLE {$table}");
+		self::query("DELETE FROM db_restore WHERE table_restore = '{$table}'");
+	}
+
+	/**
+	 * Execute a restore
+	 *
+	 * @param int $n number of operations to going back
+	 * @param int $id ID of the operation from which start
+	 * @return bool result of the operation
+	 */
+	public static function restore($n = 1,$id = NULL){
+
+		if($n < 1) $n = 1;
+		$n--;
+
+		$w = isset($id) && !empty($id) ? "WHERE id = {$id} ORDER BY id DESC " : " ORDER BY id DESC LIMIT {$n},1";
+
+		$q = self::query("SELECT * FROM db_restore {$w}");
+		if(self::count($q) == 1){
+			$a = $q -> fetch();
+			
+			self::prepare($a['table_from']);
+			
+			$q = self::_restore($a['table_from'],$a['table_restore']);
+
+			self::save();
+			return $q;
+		}
+		return false;
+	}
+
+
+	/**
+	 * Restore a table
+	 *
+	 * @param string $t1 name of table to restore
+	 * @param stirng $t2 name of table to take data
+	 * @return bool result of the query
+	 */
+	private static function _restore($t1,$t2){
+
+		return self::query("TRUNCATE table {$t1}") && 
+		self::query("INSERT {$t1} SELECT * FROM {$t2}");
+	}
 }
 ?>
